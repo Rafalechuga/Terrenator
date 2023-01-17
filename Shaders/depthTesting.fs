b7f5f817fa9e8cf82e5c9270a443ccf348c15750
Rafael <rafaga.1998@gmail.com>
2023-01-16 20:58:30 -0600

    Upload

---
+++ Shaders/depthTesting.fs
@@ -0,0 +1,21 @@
+#version 330 core
+out vec4 FragColor;
+
+float near = 0.01;
+float far = 100.0; 
+
+float linealizarProfundidad( float depth ){
+    float ndc = depth * 2 - 1;
+    return ( 2.0 * near * far ) / ( far + near - ndc * ( far - near ) );
+}
+
+void main()
+{             
+    //FragColor = vec4(1.0, 1.0, 1.0, 1.0);	
+
+    //Reenderizado de profundidad
+    // FragColor = vec4( vec3( gl_FragCoord.z ) , 1.0);
+
+    FragColor = vec4( vec3( linealizarProfundidad( gl_FragCoord.z ) / far ), 1.0 );
+
+}
